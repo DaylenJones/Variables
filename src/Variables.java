8920dbe9268e7a326b2e07c5fc21dcc9ea9701f2
Daylen Jones <jonesday001@warren.k12.in.us>
2024-07-30 13:54:28 -0400

    update

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        myFirstNumber = 6;
+        System.out.println(myFirstNumber);
+        myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+        int myfirstnumber = 5;
+
+
+    }
+
+}
